5fbb9bd2b955ae4b464f42f0f0076f86b7323731
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-04-01 19:24:50 +0100

    New Files

---
+++ Terraform_AWS_Folder_v2/rds_module/security_group.tf
@@ -0,0 +1,47 @@
+# ✅ RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS Security Group"
+  }
+}
+
+# ✅ EC2 Security Group
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "ec2-public-access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2 Security Group"
+  }
+}
